6be51a2c1f689502dbb43a2fe9346ad687182fa4
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-10 02:00:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+
+document.querySelector("main#main").remove(); 
+const newHeader = document.createElement("h1"); 
+newHeader.id = "victory";
+newHeader.innerHTML = "Amber is the champion"; 
+
+//has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
