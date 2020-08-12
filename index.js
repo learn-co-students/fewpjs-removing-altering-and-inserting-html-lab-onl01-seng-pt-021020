13296e4babd4eb39f8ee3785d467932be6bb17cf
Pablo Gonzalez <pabgon0@gmail.com>
2020-08-12 20:47:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.classname = "victory"
+newHeader.innerHTML = "Pablo Gonzalez is the champion"
+newHeader.id = "victory"
